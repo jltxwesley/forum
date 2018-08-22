79aec5f0bc4b2e2d7fd4616262a97009fa7aed6f
Xu Zhang <jltxwesley@gmail.com>
2018-08-22 22:01:34 +1000

    the reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -27,6 +27,22 @@
                 @include('threads.reply')
             @endforeach
         </div>
+
+        @if(auth()->check())
+        <div class="col-md-8">
+            <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea name="body" id="body" class="form-control" placeholder="Have something to say" rows="5"></textarea>
+                    <button type="submit" class="btn btn-default">Post</button>
+                </div>
+            </form>
+        </div>
+        @else
+        <div class="col-md-8">
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+        </div>
+        @endif
     </div>
 </div>
 @endsection
